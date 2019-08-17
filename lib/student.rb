0099756fbe58e5b42ed5cb217aa5ef88af3048b9
Amar Chatman <amarjerome@gmail.com>
2019-08-17 18:53:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,7 @@ end
 def self.find_by_name(name)
   sql = "SELECT * FROM students WHERE name = ?"
 result = DB[:conn].execute(sql, name)[0]
-Student.new(array[0], array[1], array[2])
+Student.new(result[0], result[1], result[2])
 end
 
 end
